89c9d03ae1edbfc7854e608f36f914000309a6a3
madden1706 <madden1706@gmail.com>
2018-12-06 13:56:50 +0000

    Bio Armory Intro completed.

--- Bioinformatics_Armory/Intro.py
+++ Bioinformatics_Armory/Intro.py
@@ -1,1 +1,6 @@
-from Bio import Seq
+from Bio.Seq import Seq
+
+dna = Seq("AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC")
+
+print(dna.count("A"), dna.count("C"), dna.count("G"), dna.count("T"))
+
