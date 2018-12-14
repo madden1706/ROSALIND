6719ffa63935585bf2a94fdb3ac1869db8217e5f
madden1706 <madden1706@gmail.com>
2018-12-14 10:10:16 +0000

    Updated comments to clarify function.

--- Bioinformatics_Armory/Finding_Genes_with_ORFs/Finding_Genes_with_ORFs.py
+++ Bioinformatics_Armory/Finding_Genes_with_ORFs/Finding_Genes_with_ORFs.py
@@ -4,13 +4,14 @@ from Bio import SeqIO, Seq
 input_file = "input.fasta"
 
 # getorf -sequence input.fasta -outseq output.fasta -find 1
+# Finds all ORFs starting with M.
 subprocess.run([f"getorf -sequence {input_file} -outseq output.fasta -find 1"], shell=True)
 
 length = 0
 result_seq = ""
 
 seqs = SeqIO.parse("output.fasta", "fasta")
-for x in seqs:
+for x in seqs:  # Returns longest ORF starting with M.
     if len(x.seq) > length:
         length = len(x.seq)
         result_seq = x
