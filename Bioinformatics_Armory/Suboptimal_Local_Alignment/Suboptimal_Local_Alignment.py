5d591825187d50c33ce1e4a73f71a701327ef2fb
madden1706 <madden1706@gmail.com>
2018-12-14 15:43:11 +0000

    Solved Suboptimal Local Alignment.

---
+++ Bioinformatics_Armory/Suboptimal_Local_Alignment/Suboptimal_Local_Alignment.py
@@ -0,0 +1,6 @@
+# code to find sequence that occurs in string.
+
+# Then search the result against both strings for occurrences.
+
+# Or, set gap and extensions harsh, and count occurrences on graph output.
+
